fedd4f6fcbcdd1a4ee710b113480fff854042cad
Goldmagination <62952539+Goldmagination@users.noreply.github.com>
2020-07-29 16:44:07 +0200

    some styling

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
+
   CategoryItem(this.title, this.color);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text(title),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
